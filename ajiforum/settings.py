19b15822df76ad6df18f4bd47c5aec31ec95089c
amigcamel <amigcamel@gmail.com>
2017-06-07 00:35:13 +0800

    add sample

--- ajiforum/settings.py
+++ ajiforum/settings.py
@@ -1,7 +1,7 @@
 """Django settings."""
-import os
+from os.path import dirname, abspath, join
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = dirname(dirname(abspath(__file__)))
 SECRET_KEY = 'uzi-g3ri_95le=lj6i+t)=)+y1gkay8wcj9s8&ym$d@tlzsw2d'
 DEBUG = True
 ALLOWED_HOSTS = [
@@ -30,7 +30,7 @@ ROOT_URLCONF = 'ajiforum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -49,11 +49,95 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
-TOPIC = {
-    'content': '',
-    'comments': [],
-    'upvotes': 0,
-    'downvotes': 0,
-}
 
-TOPICS = []
+import uuid
+TOPICS = [
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[翻譯] 喜劇藝人酸觀眾腦殘,結果酸錯人當場崩潰',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[kuso] 黑人頭果然百搭啊!',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[笑話] 老梗',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[猜謎] 為甚麼女生體育課不能跑步',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[猜謎]這麼多宅肥，87是誰的錯',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[猜謎] 不相連又拆不開',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[ＸＤ] 網路搞笑圖片翻譯（兩百七十二）',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[猜謎] 為什麼Skoda的車那麼好坐？',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[囧rz] 這個影片....我很難認真看下去...',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[耍冷] 藍受香菇',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'title': '[猜謎] 甲甲變性',
+        'content': '',
+        'datetime': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    },
+]
