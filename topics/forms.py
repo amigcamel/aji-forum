15b4bc3ce2fdada61fd0b2e64654c25766745033
amigcamel <amigcamel@gmail.com>
2017-06-07 03:03:03 +0800

    pep8 fix

--- topics/forms.py
+++ topics/forms.py
@@ -5,5 +5,5 @@ from django import forms
 class TopicForm(forms.Form):
     """Form for a topic."""
 
-    title = forms.CharField(label='title', max_length=100)
-    content = forms.CharField(label='content', max_length=255)
+    title = forms.CharField(label='Title', max_length=100)
+    content = forms.CharField(label='Content', max_length=255)
