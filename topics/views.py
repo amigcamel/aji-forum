ff933cdd0b28b73e2768b96f5dbfe57dfdc925a3
amigcamel <amigcamel@gmail.com>
2017-06-07 00:35:27 +0800

    add context `topics`

--- topics/views.py
+++ topics/views.py
@@ -1,12 +1,19 @@
 """Views."""
 from copy import deepcopy
 
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 from django.conf import settings
 # from django.core.urlresolvers import reverse
 
 
 def index(request):
     """Index page."""
+    return render(request, 'index.html', {'topics': settings.TOPICS})
+    TOPIC = {
+        'title': '',
+        'content': '',
+        'upvotes': 0,
+        'downvotes': 0,
+    }
     settings.TOPICS.append(deepcopy(settings.TOPIC))
     return HttpResponse(settings.TOPICS)
