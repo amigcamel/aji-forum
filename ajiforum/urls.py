3073f43f62c89ce3357db59d4822f8b50209952f
amigcamel <amigcamel@gmail.com>
2017-06-07 01:30:07 +0800

    add route for topics.views.vote

--- ajiforum/urls.py
+++ ajiforum/urls.py
@@ -4,4 +4,9 @@ import topics.views
 
 urlpatterns = [
     url(r'^$', topics.views.index, name='index'),
+    url(
+        r'(?P<uuid>[a-zA-Z0-9-]{36})/(?P<target>(upvotes|downvotes))$',
+        topics.views.vote,
+        name='vote'
+    ),
 ]
